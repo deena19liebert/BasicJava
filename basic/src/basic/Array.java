ff9eaa70d869dadb6e5b5d94378e8b206bf3e924
deena19liebert <deena.t.a.2023.cse@ritchennai.edu.in>
2024-07-28 18:00:43 +0530

    Initial Commit

--- basic/src/basic/Array.java
+++ basic/src/basic/Array.java
@@ -3,8 +3,17 @@ package basic;
 public class Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	  int size = 5;
+	  
+	  for( int i=0; i<size; i++) {
+        for( int j = 0; j < size; j++) {
+        	if( i == j || i + j == size - 1) {
+        	System.out.print("0");
+        } else {
+        	System.out.print(" ");
+        }
+        }
+        	System.out.println(" ");
+        }
+	  }
 	}
-
-}
